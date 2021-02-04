37e64f8916e9d21e26fbce47b7891089537d5a2c
Hovhannes23 <twinkle.groupbz@gmail.com>
2021-02-04 15:36:15 +0300

    Initial commit

---
+++ src/main/java/phone/number/cheker/demo/PhoneNumberChekerApplication.java
@@ -0,0 +1,13 @@
+package phone.number.cheker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNumberChekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneNumberChekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phone/number/cheker/demo/PhoneNumberChekerApplicationTests.java
@@ -0,0 +1,13 @@
+package phone.number.cheker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumberChekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
